e8d9a03f4105490bb6fa3124a141a9bc84c4f79c
diego <ruso439@gmail.com>
2022-01-03 11:38:49 -0300

    sin cambios realizados

---
+++ javascript  curso/app.js
@@ -0,0 +1,26 @@
+// tipos de datos
+"hello world" // string
+
+100000 // number
+
+// boolean
+true
+false
+
+// array
+/*[´joe´, ´ryan´, ´martha´]
+[true,false,true,false]
+
+// object
+70.4
+14
+true
+{
+    "username": ryan,
+    "score"; 70.4,
+    "hours": 14,
+    "proffesional": true
+    "friends":
+
+}
+*/
